1988f2dd2a4730c1ac2ed42b36a74cfb70b92a16
John Bellone <jbellone@bellone.us>
2016-07-24 20:53:00 -0400

    Add sudo default recipe for /etc/sudoers fix.

--- test/fixtures/policies/_base.rb
+++ test/fixtures/policies/_base.rb
@@ -1,7 +1,7 @@
 default_source :community
 default_source :chef_repo, '..'
 cookbook 'redis', path: '../../..'
-run_list 'redis::default'
+run_list 'sudo::default', 'redis::default'
 named_run_list :centos, 'yum::default', 'yum-epel::default', run_list
 named_run_list :debian, 'apt::default', run_list
 named_run_list :freebsd, 'freebsd::default', run_list
